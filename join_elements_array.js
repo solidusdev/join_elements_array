9330dd994ff8b4a98eb84f765626c260f99d1f80
Ivan <i_cuenca@aol.com>
2020-01-20 11:51:06 -0500

    first commit

---
+++ join_elements_array.js
@@ -0,0 +1,7 @@
+let sample_array_1 = ["Red", "Green", "White", "Black"];
+let sample_array_2 = ["HTML", "CSS", "JavaScript"];
+
+let join_elements = arr => arr.join(", ");
+
+console.log(join_elements(sample_array_1)); // Red, Green, White, Black
+console.log(join_elements(sample_array_2)); // HTML, CSS, JavaScript
